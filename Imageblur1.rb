368148854f82ec88a3983fd25fc41feb8da92804
Christian Wigert <christian.wigert@gmail.com>
2019-09-15 16:25:06 +0000

    completed Image blur 1 coding challenge

--- Imageblur1.rb
+++ Imageblur1.rb
@@ -1,3 +1,26 @@
-class image 
+class Image
+  attr_accessor :image
+  def initialize(image)
+    @image = image
+  end
 
-end 
+  def output_image
+    image.each do |row|
+      row.each do |cell|
+        puts cell
+       end
+    end
+  end
+end
+
+
+
+image = Image.new([
+  ["0, 0, 0, 0"],
+  ["0, 1, 0, 0"],
+  ["0, 0, 0, 1"],
+  ["0, 0, 0, 0"]
+])
+image.output_image
+
+
